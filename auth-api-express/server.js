06fd5efa6160016d9fd9793593b877a7fbf265fb
Juan Yepes <juanferhiguita65@gmail.com>
2026-06-15 21:27:26 -0500

    Fix: fixed controllers folder name and server routes

--- auth-api-express/server.js
+++ auth-api-express/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+    console.log("Servidor ejecutándose en puerto 3000");
 });
